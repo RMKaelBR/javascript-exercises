57736d5f9195499ce93c9f365f3f4b0498ef1e85
RMKaelBR <rmbresurreccion@gmail.com>
2023-08-21 20:18:49 +0800

    accomplish 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
 const sumAll = function() {
+  if (typeof arguments[0] != "number" || typeof arguments[1] != "number")
+    return "ERROR"
+  else if (arguments[0] < 0 || arguments[1] < 0)
+    return "ERROR"
+  
+  if (arguments[0] > arguments[1]) {
+    lowerLimit = arguments[1];
+    upperLimit = arguments[0];
+  }
+  else {
+    lowerLimit = arguments[0];
+    upperLimit = arguments[1];
+  }
 
+  let sum = 0
+  for (let i = lowerLimit; i <= upperLimit; i++)
+    sum += i;
+  return sum;
 };
 
 // Do not edit below this line

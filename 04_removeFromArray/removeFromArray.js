41c46c5d56a018e631f7005ec012c3b0b8a8c632
RMKaelBR <rmbresurreccion@gmail.com>
2023-08-21 19:57:44 +0800

    accomplish 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
 const removeFromArray = function() {
-
+  realArray = Array.from(arguments)
+  // console.log(arguments[0])
+  arguments[0] = arguments[0].filter((element) => {
+    if (!realArray.includes(element)) 
+      return element;
+    });
+  // console.log(arguments[0])
+  return arguments[0];
 };
 
 // Do not edit below this line

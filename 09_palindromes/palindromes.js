6240c3850c789c205241725aaebd24b14671c7fa
RMKaelBR <rmbresurreccion@gmail.com>
2023-08-23 17:38:19 +0800

    accomplish 09_exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
 const palindromes = function () {
-
+  let string = arguments[0].toLowerCase().split("")
+  string = string.filter((letter) => {
+    if (letter.match(/[a-z0-9]/i))
+      return letter;
+  })
+  stringOriginal = string.join()
+  stringReversed = string.reverse().join()
+  if (string == stringOriginal)
+    return true
+  else
+    return false
 };
 
 // Do not edit below this line

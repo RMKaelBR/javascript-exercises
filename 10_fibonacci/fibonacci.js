986da01ac2c106573659fab0b121e0962ba62e3a
RMKaelBR <rmbresurreccion@gmail.com>
2023-08-23 18:11:53 +0800

    accomplish 10_exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,24 @@
-const fibonacci = function() {
 
+const fibonacci = function(fibMax, prevFib=1, currentFib=2, fibTracker=3) {
+  fibMax = parseInt(fibMax)
+  
+  if (fibMax >= 3) {
+    if (fibTracker < fibMax) {
+      fibTracker++;
+      let fibHolder = currentFib
+      currentFib += prevFib
+      prevFib = fibHolder
+      return currentFib = fibonacci(fibMax, prevFib, currentFib, fibTracker);
+    }
+    else
+      return currentFib
+  }
+  else if (fibMax > 0)
+    return 1;
+  else
+    return "OOPS";
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+// 1, 1, 2, 3, 5, 8
